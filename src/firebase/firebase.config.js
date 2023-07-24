69f35c19ffe2a29eed584cff611e9c87d4a7b074
Nirob Hasan <nirob427417@gmail.com>
2023-07-24 12:50:55 +0600

    next auth

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPWfS0Xo-WFJiR5vZx7ez7-r013IW5WKU",
+  authDomain: "spry-tree-393804.firebaseapp.com",
+  projectId: "spry-tree-393804",
+  storageBucket: "spry-tree-393804.appspot.com",
+  messagingSenderId: "143308729422",
+  appId: "1:143308729422:web:df64a45e00e69478f3671f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,15 +1,20 @@
 import { Avatar } from "antd";
-import { UserOutlined } from "@ant-design/icons";
+import { useSession } from "next-auth/react";
 
 import Head from "next/head";
 const ProfilePage = () => {
+  const { data: session } = useSession();
+  console.log(session);
   return (
     <div style={{ textAlign: "center" }}>
       <Head>
         <title>Next Profile</title>
       </Head>
       <h1>User Profile</h1>
-      <Avatar size={64} icon={<UserOutlined />} />
+      <Avatar size={64} src={session?.user?.image} />
+      {/* <Avatar size={64} icon={<UserOutlined />} /> */}
+      <h2 style={{ textAlign: "center" }}>Name: {session?.user?.name} </h2>
+      <h4 style={{ textAlign: "center" }}>Email: {session?.user?.email} </h4>
     </div>
   );
 };
